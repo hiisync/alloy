66a034a7c1fb06be9deaf8ff3baf22357913c0bc
Mikola <60260972+hiisync@users.noreply.github.com>
2024-03-01 00:16:07 +0200

    Added CORS

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 #[macro_use]
 extern crate rocket;
 use dotenvy::dotenv;
+use rocket_cors::{AllowedOrigins, CorsOptions};
 
 pub mod handlers;
 
 #[launch]
 fn rocket() -> _ {
     dotenv().ok();
+
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::some_exact(&[
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+        ]))
+        .to_cors()
+        .expect("Error creating CORS fairing");
+
     rocket::build()
+        .attach(cors)
         .mount(
             "/posts",
             routes![handlers::post::list, handlers::post::create_post],
@@ -20,7 +31,7 @@ fn rocket() -> _ {
                 handlers::user::auth,
                 handlers::user::me,
                 handlers::user::get,
-                handlers::user::verify
+                handlers::user::verify,
             ],
         )
 }
