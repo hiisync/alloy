d38abce4253d0505726cde5fcf92ac31dc45bafb
Savchin <stiv.asakura@gmail.com>
2024-05-09 00:01:39 +0300

    modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use core::panic;
 use diesel::prelude::*;
 use diesel::{Connection, PgConnection as DB};
-use dotenvy::dotenv;
 use std::env;
 
 pub mod middleware;
@@ -9,9 +8,8 @@ pub mod models;
 pub mod schema;
 
 pub fn connect_db() -> DB {
-    dotenv().ok();
-
     let database_url: String = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    
     DB::establish(&database_url)
         .unwrap_or_else(|e: ConnectionError| panic!("Error connecting: {}", e))
 }
